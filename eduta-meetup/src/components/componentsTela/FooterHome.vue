1cb7e7997264236b567a77fb7ddf1399d86a9f63
Jobson José de O. R. Toscano de Brito <jjbrito@fitec.org.br>
2020-07-21 23:15:38 -0300

    footer atualizado version de estrutura fechada

--- eduta-meetup/src/components/componentsTela/FooterHome.vue
+++ eduta-meetup/src/components/componentsTela/FooterHome.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="container-footer">
-        <div class="col-principal"></div>
-        <div class="col-secudaria"></div>
+        <div class="icon-footer">
+            <img src="https://image.flaticon.com/icons/svg/265/265754.svg" alt="">
+        </div>
+        <div class="col-main">
+            <ul>
+                <li class="title-list">Projetistas</li>
+                <li>Apoiador</li>
+                <li>Projetos</li>
+                <li>Como criar projetos ?</li>
+            </ul>
+        </div>
+        <div class="col-secundary">
+            <ul>
+                <li class="title-list">Informações</li>
+                <li>Deseja Trabalhar conosco ?</li>
+                <li>Sobre</li>
+                <li>Patrocinios</li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
@@ -15,5 +32,31 @@ export default {
 }
 </script>
 <style scoped>
-
+.container-footer div{
+    float: left;
+}
+.container-footer{
+    width: 96%;
+    float: left;
+    background: #d82e52;
+    padding: 2%;
+    color: white;
+}
+li{
+    list-style-type: none;
+    padding: 5%
+}
+.col-main, .col-secundary{
+    width: 25%;
+    margin: 3% 5%;
+}
+.icon-footer{
+    width: 10%;
+    padding: 4%
+}
+.title-list{
+    font-size:large;
+    border-bottom: 1.8px solid white;
+    padding: 3% !important;
+}
 </style>
--- eduta-meetup/src/pages/Home.vue
+++ eduta-meetup/src/pages/Home.vue
@@ -4,6 +4,7 @@
     <row-design></row-design>
     <info-home></info-home> 
     <about-home></about-home>
+    <footer-home></footer-home>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import bannerHome from '@/components/componentsTela/BannerHome.vue';
 import rowDesign from '@/components/componentsDesign/Row.vue';
 import informativeHome from '@/components/componentsTela/InformativeHome.vue';
 import aboutHome from '@/components/componentsTela/AboutHome.vue';
+import footerHome from '@/components/componentsTela/FooterHome.vue';
 
 export default {
   name: 'Home',
@@ -19,7 +21,8 @@ export default {
     'banner-home': bannerHome,
     'row-design': rowDesign,
     'info-home': informativeHome,
-    'about-home': aboutHome
+    'about-home': aboutHome,
+    'footer-home': footerHome
   },
   data () {
     return {
